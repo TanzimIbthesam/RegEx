85d534d615cb8d79b0af91a0dc01457e75d8bb46
Iamtanzim <tanzim11@gmail.com>
2020-03-27 22:03:21 +0600

    Lesson15 commited

---
+++ 15-Finishingtouches/script.js
@@ -0,0 +1,36 @@
+const inputall=document.querySelectorAll('input');
+const para=document.querySelectorAll('p');
+console.log(para);
+
+const patterns = {
+    telephone: /^\d{11}$/,
+    username:/^[a-z\d]{5,12}$/i,
+    password:/^[\w@-]{8,20}$/,
+    slug:/^[a-z\d-]{8,20}$/,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+        //             yourname @ domain   .  com          ( .uk )
+};
+
+
+const validate=(field,regex)=>{
+    console.log(regex.test(field.value));
+    if(regex.test(field.value)){
+        field.className='Valid';
+
+    }else{
+        field.className='Invalid'; 
+    }
+}
+
+inputall.forEach((input) => {
+    input.addEventListener('keyup',(e)=>{
+        //    e.preventDefault();
+        //   console.log(e.target.attributes.name.value);
+       para.forEach(p => {
+           p.style="display:block";
+           
+       });
+        validate(e.target,patterns[e.target.attributes.name.value]);
+    });
+    
+});
